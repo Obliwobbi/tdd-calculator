31b50a3bc55b6ee0c5ef0be2398879c97c4c8432
Obliwobbi <tobiashartzberg@gmail.com>
2025-08-21 15:22:26 +0200

    Initial commit - add to repo

---
+++ src/main/java/dk/obli/tdd/Calculator.java
@@ -0,0 +1,8 @@
+package dk.obli.tdd;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/dk/obli/tdd/CalculatorTest.java
@@ -0,0 +1,19 @@
+package dk.obli.tdd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    
+}
